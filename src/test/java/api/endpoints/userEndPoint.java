6b1880783e77baf6bda8ee3129ee369f1133b6ab
Vvarun0 <varun07nurav@gmail.com>
2025-07-30 00:32:10 +0530

    Added Files

--- src/test/java/api/endpoints/userEndPoint.java
+++ src/test/java/api/endpoints/userEndPoint.java
@@ -1,4 +1,60 @@
 package api.endpoints;
 
-public class UserEndPoint {
+import api.payload.user;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+
+import static api.endpoints.Routes.*;
+import static io.restassured.RestAssured.given;
+
+public class userEndPoint {
+
+public static Response CreateUser(user payload){
+
+    Response response =given()
+            .accept(ContentType.JSON)
+            .contentType(ContentType.JSON)
+            .body(payload)
+
+            .when()
+            .post(post_url);
+    return response;
+
+}
+    public static Response GetUser(String userName){
+
+        Response response =given()
+                .accept(ContentType.JSON)
+                .pathParam("username",userName)
+
+                .when()
+                .get(get_url);
+        return response;
+
+    }
+    public static Response UpdateUser(String userName, user payload){
+
+        Response response =given()
+                .accept(ContentType.JSON)
+                .contentType(ContentType.JSON)
+                .pathParam("username",userName)
+                .body(payload)
+
+                .when()
+                .put(put_url);
+        return response;
+
+    }
+    public static Response DeleteUser(String userName){
+
+        Response response =given()
+                .accept(ContentType.JSON)
+                .pathParam("username",userName)
+
+
+                .when()
+                .delete(delete_url);
+        return response;
+
+    }
 }
--- src/test/java/api/testcases/UserTest.java
+++ src/test/java/api/testcases/UserTest.java
@@ -1,4 +1,78 @@
 package api.testcases;
 
+import api.endpoints.userEndPoint;
+import api.payload.user;
+import com.github.javafaker.Faker;
+import io.restassured.response.Response;
+import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
 public class UserTest {
+
+    Faker faker;
+    user userPayload;
+
+    @BeforeClass
+    public void generateTestData(){
+        faker = new Faker();
+        userPayload = new user();
+
+        userPayload.setId(faker.idNumber().hashCode());
+        userPayload.setUsername(faker.name().username());
+        userPayload.setFirstName(faker.name().firstName());
+        userPayload.setLastName(faker.name().lastName());
+        userPayload.setEmail(faker.internet().safeEmailAddress());
+        userPayload.setPassword(faker.internet().password(5, 10));
+        userPayload.setPhone(faker.phoneNumber().cellPhone());
+
+    }
+    @Test(priority = 1)
+    public void testCreateUser(){
+        Response response = userEndPoint.CreateUser(userPayload);
+        System.out.println("CREATE USER ");
+        //log response
+        response.then().log().all();
+
+        //validation
+        Assert.assertEquals(response.getStatusCode(),200);
+    }
+    @Test(priority = 2)
+    public void testgetUser(){
+        Response response = userEndPoint.GetUser(this.userPayload.getUsername());
+        System.out.println("GET USER DATA ");
+        //log response
+        response.then().log().all();
+
+        //validation
+        Assert.assertEquals(response.getStatusCode(),200);
+    }
+    @Test(priority = 3)
+    public void testUpdateUser(){
+        userPayload.setFirstName(faker.name().firstName());
+        Response response = userEndPoint.UpdateUser(this.userPayload.getUsername(),userPayload);
+
+        System.out.println("UPDATE USER DATA");
+        //log response
+        response.then().log().all();
+
+        //validation
+        Assert.assertEquals(response.getStatusCode(),200);
+        System.out.println("CHECK THE RESPONSE POST UPDATE");
+        System.out.println("UPDATED USER DETAILS : "+ response.getBody().prettyPrint());
+    }
+    @Test(priority = 4)
+    public void testDeleteUser(){
+
+        Response response = userEndPoint.DeleteUser(this.userPayload.getUsername());
+
+        System.out.println("DELETE USER DATA");
+        //log response
+        response.then().log().all();
+
+        //validation
+        Assert.assertEquals(response.getStatusCode(),200);
+    }
+
+
 }
